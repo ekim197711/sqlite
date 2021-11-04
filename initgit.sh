14e90732c07941d21ea7b55acdef595c7b9e4d27
Mike Møller Nielsen <ekim197711@gmail.com>
2021-11-04 16:59:32 +0100

    files

---
+++ initgit.sh
@@ -0,0 +1,7 @@
+echo "# sqlite" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:ekim197711/sqlite.git
+git push -u origin main
